faabb07a0b0aeda86d9c8dba8aeb49fcf32dd252
lucastx <lucas@lucasteixeira.com>
2012-04-21 15:32:59 -0300

    tirei extensão do README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['tjrj',],
     include_package_data = True,
     license='GPLv2',
-    long_description=open('README.txt').read(),
+    long_description=open('README').read(),
 
     entry_points = {
         'console_scripts': [
